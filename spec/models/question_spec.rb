328fbc279c44c9f8f52ecfecf5bf8d96e7dec789
Hakimu Jackson <hakimu@newrelic.com>
2018-09-20 23:26:36 -0700

    Added a few rspec tests

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+
+  it "is only valid when there is a body to the question." do
+    question = Question.new(body: "What is your name?")
+    expect(question).to be_valid
+  end
+
+  it "is not valid unless there is a body" do
+    question = Question.new(body: nil)
+    question.valid?
+    expect(question.errors[:body]).to include("can't be blank")
+  end
+
+
+end
---
+++ spec/models/survey_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Survey, type: :model do
+  
+  it "is valid when there is a title" do
+    survey = Survey.new(title: "first_survey")
+    expect(survey).to be_valid
+  end
+
+  it "is not valid when there is no title" do
+    survey = Survey.new(title: nil)
+    survey.valid?
+    expect(survey.errors[:title]).to include("can't be blank")
+  end
+end
