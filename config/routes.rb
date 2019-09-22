b49279273522c3cab421ddeddbd699c0b3fb0823
Hakimu Jackson <hakimu@newrelic.com>
2019-09-22 14:14:38 -0700

    Add Sessions and Users controller for login / logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
   resources :answers
   resources :questions
   resources :surveys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
