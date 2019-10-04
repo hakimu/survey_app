ac4f431c406fdeb000ada966edf52da6b9fc9c41
Hakimu Jackson <hakimu@newrelic.com>
2019-10-03 22:25:29 -0700

    Spec refactor

--- spec/factories/responses.rb
+++ spec/factories/responses.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :response do
-    user { nil }
-    answer { nil }
+    user
+    answer
   end
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Response, type: :model do
   it 'needs a user to be valid' do
-    answer = create(:answer)
-    response = build(:response, user_id: nil, answer_id: answer.id)
+    response = build(:response, user_id: nil)
     response.save
     expect(response.errors[:user]).to include('can\'t be blank')
   end
   it 'needs an answer to be valid' do
-    user = create(:user)
-    response = build(:response, user_id: user.id, answer_id: nil)
+    response = build(:response, answer_id: nil)
     response.save
     expect(response.errors[:answer]).to include('can\'t be blank')
   end
