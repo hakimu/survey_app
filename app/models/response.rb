aae1c5cae58f2df6dab7e196ee5830170f07f044
Hakimu Jackson <hakimu@newrelic.com>
2019-10-03 22:14:29 -0700

    Add initial model spec for response

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :user
   belongs_to :answer
+  validates :user, presence: true
+  validates :answer, presence: true
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Response, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'needs a user to be valid' do
+    answer = create(:answer)
+    response = build(:response, user_id: nil, answer_id: answer.id)
+    response.save
+    expect(response.errors[:user]).to include('can\'t be blank')
+  end
+  it 'needs an answer to be valid' do
+    user = create(:user)
+    response = build(:response, user_id: user.id, answer_id: nil)
+    response.save
+    expect(response.errors[:answer]).to include('can\'t be blank')
+  end
 end
